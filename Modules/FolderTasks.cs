ce67bebb5892a58850e5a01971420c54423da4fd
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-04-04 18:02:22 +0600

    Design of workflow

--- Modules/FolderTasks.cs
+++ Modules/FolderTasks.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FilenFolderManager.Models
+namespace FilenFolderManager.Modules
 {
     internal class FolderTasks
     {
--- Program.cs
+++ Program.cs
@@ -1,17 +1,13 @@
-﻿namespace FilenFolderManager
+﻿using FilenFolderManager.Modules;
+
+namespace FilenFolderManager
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            //TEST
-            string currDir = Directory.GetCurrentDirectory();
-            string[] filesAndFolders = Directory.GetFileSystemEntries(currDir);
-
-            foreach(string fileOrFolder in filesAndFolders)
-            {
-                Console.WriteLine(fileOrFolder);
-            }
+            InputProcessor inP = new InputProcessor();
+            inP.Process();
         }
     }
 }
