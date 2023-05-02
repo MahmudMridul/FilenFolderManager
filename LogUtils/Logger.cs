184d4d2b8f23ceb79ddba30ff115235f5c4fe6c4
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-05-02 16:08:27 +0600

    Keep logs of last 7 days

--- LogUtils/Logger.cs
+++ LogUtils/Logger.cs
@@ -10,6 +10,7 @@ namespace FilenFolderManager.LogUtils
             .WriteTo.File(
                 "Logs/log.txt",
                 rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 7,
                 outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] ({SourceContext}) {Message:lj}{NewLine}{Exception}"
             );
         
