f938c20b1dd5ff24d06b662689a46d5921a811e2
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-05-02 15:32:34 +0600

    Class name and method name in log

--- LogUtils/Logger.cs
+++ LogUtils/Logger.cs
@@ -1,22 +1,29 @@
 ﻿using Serilog;
+using Serilog.Context;
 
 namespace FilenFolderManager.LogUtils
 {
     internal class Logger
     {
-        private static readonly Logger Instance = new Logger();
-        private readonly ILogger _logger;
+        private static readonly LoggerConfiguration _configuration = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.File(
+                "Logs/log.txt",
+                rollingInterval: RollingInterval.Day,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] ({SourceContext}) {Message:lj}{NewLine}{Exception}"
+            );
+        
+        private static readonly ILogger _logger = _configuration.CreateLogger();
 
-        private Logger()
+        public static void Info(string message)
         {
-            _logger = new LoggerConfiguration()
-                .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-        }
+            var className = new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().DeclaringType.Name;
+            var methodName = new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().Name;
 
-        public static void Info(string messsage)
-        {
-            Instance._logger.Information(messsage);
+            using (LogContext.PushProperty("SourceContext", $"{className}.{methodName}"))
+            {
+                _logger.Information(message);
+            }
         }
 
     }
